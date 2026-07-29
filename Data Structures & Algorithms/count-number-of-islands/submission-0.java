af924061a86af72d3a4584adec7b30dc3c362b14
Hafsa Usman <hafsausman1512@gmail.com>
2026-07-29 16:34:20 +0500

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.java
@@ -0,0 +1,39 @@
+class Solution {
+    public int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int islandCount = 0;
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (grid[r][c] == '1') {
+                    islandCount++;
+                    Stack<int[]> stack = new Stack<>();
+                    stack.push(new int[] {r, c});
+                    grid[r][c] = '-';
+
+                    while (!stack.isEmpty()) {
+                        int[] curr = stack.pop();
+                        int currR = curr[0];
+                        int currC = curr[1];
+
+                        int[][] directions = {{-1, 0}, {1, 0}, {0, 1}, {0, -1}};
+                        for (int[] dir : directions) {
+                            int nr = currR + dir[0];
+                            int nc = currC + dir[1];
+                            if (nr >= 0 && nr < rows && nc >= 0 && nc < cols
+                                && grid[nr][nc] == '1') {
+                                grid[nr][nc] = '-';
+                                stack.push(new int[] {nr, nc});
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return islandCount;
+    }
+}
